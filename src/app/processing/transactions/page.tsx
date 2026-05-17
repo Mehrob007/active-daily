62f49428e914da79ab9aa214d0f4edd7edf8ce2c
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-05-17 19:48:11 +0500

    refactor: update transaction page import path and refresh processing navigation icons

--- src/app/processing/transactions/page.tsx
+++ src/app/processing/transactions/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import TransactionsPage from '@/features/technical/TransactionsPage';
+import TransactionsPage from '@/features/processing/pages/TransactionsPage';
 
 export default function Page() {
   return <TransactionsPage />;
